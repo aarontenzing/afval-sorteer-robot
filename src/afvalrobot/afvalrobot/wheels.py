d716332f51504158a8f92050694bb2aeac922a84
Aaron <aarontenzing@gmail.com>
2024-04-20 14:04:01 +0200

    wheels update

--- src/afvalrobot/afvalrobot/wheels.py
+++ src/afvalrobot/afvalrobot/wheels.py
@@ -6,7 +6,7 @@ from geometry_msgs.msg import Twist
 class Wheels(Node):
 
     def __init__(self):
-        super().__init__('wheels')
+    	super().__init__('wheels')
 		self.currentState = 0
 		self.stateSubscription = self.create_subscription(
 			Int8,
@@ -20,36 +20,36 @@ class Wheels(Node):
 			1
         ) 
     
-def controlWheels(self, state):
-	linear, angular = 0.0, 0.0
-	# STRAIGHT
-	if state == 0:
-		linear, angular = 1.0, 0.0
-	# RIGHT
-	elif state == 1:
-		linear, angular = 0.0, -1.0
-	# LEFT
-	elif state == 2:
-		linear, angular = 0.0, 1.0
-	# BACK 
-    elif state == 3:
-        linear, angular = -1.0, 0.0
-    else:
-        linear, angular = 0.0, 0.0
-    
-    cmd = Twist()
-    cmd.linear.x =  linear
-    cmd.angular.z = angular
-
-    self.wheelsPublisher.publish(cmd)
+	def controlWheels(self, state):
+		linear, angular = 0.0, 0.0
+		# STRAIGHT
+		if state == 0:
+			linear, angular = 1.0, 0.0
+		# RIGHT
+		elif state == 1:
+			linear, angular = 0.0, -1.0
+		# LEFT
+		elif state == 2:
+			linear, angular = 0.0, 1.0
+		# BACK 
+		elif state == 3:
+			linear, angular = -1.0, 0.0
+		else:
+			linear, angular = 0.0, 0.0
 		
-    
-def state_callback(self, msg):
-	self.currentState = msg.data
-	self.get_logger().info('I heard state: "%s"' % msg.data)
+		cmd = Twist()
+		cmd.linear.x =  linear
+		cmd.angular.z = angular
 
-	#Modify depending on states
-	controlWheels(self.currentState) 
+		self.wheelsPublisher.publish(cmd)
+			
+		
+	def state_callback(self, msg):
+		self.currentState = msg.data
+		self.get_logger().info('I heard state: "%s"' % msg.data)
+
+		#Modify depending on states
+		controlWheels(self.currentState) 
 	
 def main(args=None):
 	rclpy.init(args=args)
