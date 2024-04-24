be6e2c324336b8f70b5a6ead6b70464a1b37e3c8
Aaron <aarontenzing@gmail.com>
2024-04-24 14:13:25 +0200

    search

--- src/afvalrobot/afvalrobot/wheels.py
+++ src/afvalrobot/afvalrobot/wheels.py
@@ -1,7 +1,8 @@
 import rclpy
 from rclpy.node import Node
-from std_msgs.msg import Int32
+from std_msgs.msg import Int32, Float32
 from geometry_msgs.msg import Twist
+import time
 
 # MAX linear speed = 0.22 // m/s
 # MAX angular speed = 2.84 // rad/s
@@ -13,10 +14,32 @@ class Wheels(Node):
 		self.currentState = 0
 		self.stateSubscription = self.create_subscription(Int32, 'currentState', self.state_callback, 1)		   
 		self.wheelsPublisher = self.create_publisher(Twist, '/cmd_vel', 1) 
+		self.distanceSubcription = self.create_subscription(Float32, 'distance', self.distance_callback, 1)
 	
+	def search(self):
+		cmd = Twist()
+		while(self.distance_callback > 10):
+			# drive forward 5 seconds
+			cmd.linear.x, cmd.angular.z = 0.1, 0.0
+			self.wheelsPublisher.publish(cmd)
+			time.sleep(5)
+			# turn for 1 second
+			cmd.linear.x, cmd.angular.z = 0.0, 1.0
+			self.wheelsPublisher.publish(cmd)
+			time.sleep(1)
+		
+		# turn for 3 second 
+		cmd.linear.x, cmd.angular.z = 0.0, 1.0
+		self.wheelsPublisher.publish(cmd)
+		time.sleep(3)
+
 	def controlWheels(self, state):
+		
+		# Search object
 		if state == 0:
-			linear, angular = 0.0, 0.0
+			while (1):
+				self.search()
+
 		# FORWARD
 		elif state == 1:
 			linear, angular = -0.1, 0.0
@@ -31,12 +54,6 @@ class Wheels(Node):
 			linear, angular = 0.1, 0.0
 		else:
 			linear, angular = 0.0, 0.0
-		
-		cmd = Twist()
-		cmd.linear.x =	linear
-		cmd.angular.z = angular
-
-		self.wheelsPublisher.publish(cmd)
 			
 		
 	def state_callback(self, msg):
@@ -45,6 +62,10 @@ class Wheels(Node):
 
 		#Modify depending on states
 		self.controlWheels(self.currentState) 
+
+	def distance_callback(self, msg):
+		self.distance = msg.data
+		self.get_logger().info('I heard distance: "%s"' % msg.data)
 	
 def main(args=None):
 	rclpy.init(args=args)
