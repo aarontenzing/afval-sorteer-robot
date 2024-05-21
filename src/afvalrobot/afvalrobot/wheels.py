4e8870bf9bc8e76ffec4be38bca83f5845cb00d6
tdw <tdw@ubuntutdw.myguest.virtualbox.org>
2024-05-21 13:40:29 +0200

    Fixed debug counter

--- src/afvalrobot/afvalrobot/wheels.py
+++ src/afvalrobot/afvalrobot/wheels.py
@@ -117,7 +117,7 @@ class Wheels(Node):
 			
 			elif self.camera == "not":
 				self.counter += 1
-				self.get_logger().info('I count not' + counter)
+				self.get_logger().info('I count not' + str(self.counter))
 
 			# find trash
 			if self.counter >= 5 and self.currentState == 1:
